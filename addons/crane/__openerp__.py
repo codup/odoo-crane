2f77f1b7485a9b6e231b5f561c4123bbac179ce4
codup <codup@mail.ru>
2014-09-30 21:57:20 +0400

    move from bazaar

---
+++ addons/crane/__openerp__.py
@@ -0,0 +1,56 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 CodUP (<http://codup.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Crane & Hoist',
+    'version': '1.0',
+    'summary': 'Inspection and Service System',
+    'description': """
+Inspection and Service System for Crane & Hoist.
+================================================
+Support following feature:
+    * Manage Customers
+    * Manage Equipment
+    * Work Orders and Tasks
+    * Inspection and Service
+    * Work Order and Inspection Report
+    """,
+    'author': 'CodUP',
+    'website': 'http://codup.com',
+    'category': 'Enterprise Asset Management',
+    'sequence': 0,
+    'depends': ['report_webkit','mail','email_template'],
+    'demo': ['crane_demo.xml'],
+    'data': [
+        'wizard/repeat_work_order_view.xml',
+        'wizard/see_photo.xml',
+        'crane_view.xml',
+        'users_view.xml',
+        'res_config_view.xml',
+        'crane_sequence.xml',
+        'crane_data.xml',
+        'crane_report.xml',
+        'wizard/email_work_order_data.xml',
+    ],
+    'css': ['static/src/css/crane.css'],
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ addons/crane/report/work_order.py
@@ -0,0 +1,39 @@
+﻿# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2014 CodUP (<http://codup.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+import time
+from report import report_sxw
+
+class work_order(report_sxw.rml_parse):
+    def __init__(self, cr, uid, name, context):
+        super(work_order, self).__init__(cr, uid, name, context=context)
+        self.localcontext.update({
+            'time': time,
+            'cr':cr,
+            'uid': uid,
+        })
+
+report_sxw.report_sxw('report.crane.work.order.webkit',
+                      'crane.work.order',
+                      'crane/report/work_order.html',
+                      parser=work_order)
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
